a9626f2430bf9bd88db7d984e7edaf74fa53a38a
sijibomi <gbemilesanmi@gmail.com>
2023-09-27 02:55:30 +0100

    chore: fixed shadcdn && tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 //import Image from 'next/image'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="min-h-screen flex-col items-center  p-24 bg-slate-700">
             <p>Hello world</p>
+            <Button variant="secondary">Click me bitch</Button>
         </main>
     )
 }
