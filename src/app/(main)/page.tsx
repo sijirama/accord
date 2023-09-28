c5a6972514a2bf82c88e72e27a04a8c2ade9e203
sijibomi <gbemilesanmi@gmail.com>
2023-09-28 01:16:12 +0100

    chore: UserButton for clerk auth

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 //import Image from 'next/image'
 import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
     return (
         <main className="min-h-screen flex-col items-center  p-24 bg-slate-700">
             <p className="font-bold ">This is a protected route!</p>
             <Button variant="default">Click me bitch</Button>
+            <UserButton afterSignOutUrl="/signin" />
         </main>
     )
 }
