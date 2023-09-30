ff13a016a8661459a9a7c8b7c6dc22e2f21096d5
sijibomi <gbemilesanmi@gmail.com>
2023-09-30 04:39:14 +0100

    prisma setup for yad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs"
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     return (
         <ClerkProvider>
             <html lang="en" suppressHydrationWarning>
-                <body className={font.className}>
+                <body className={
+                    cn(
+                        font.className,
+                        "bg-slate-300 dark:bg-gray-900")}>
                     <ThemeProvider
                         attribute='class'
                         defaultTheme='dark'
