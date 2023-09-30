046ecd88de9b33c0fc756d12008cc7a53908edfb
sijibomi <gbemilesanmi@gmail.com>
2023-09-30 03:05:49 +0100

    chore: mode thme toggler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs"
+import { ThemeProvider } from '@/components/providers/theme-provider'
 
 const font = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,18 @@ export default function RootLayout({
 }) {
     return (
         <ClerkProvider>
-            <html lang="en">
-                <body className={font.className}>{children}</body>
+            <html lang="en" suppressHydrationWarning>
+                <body className={font.className}>
+                    <ThemeProvider
+                        attribute='class'
+                        defaultTheme='dark'
+                        //forcedTheme='light'
+                        enableSystem={false}
+                        storageKey='discord-theme'
+                    >
+                        {children}
+                    </ThemeProvider>
+                </body>
             </html>
         </ClerkProvider>
     )
