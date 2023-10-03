b6c8c86b03fd90d8fbb477d0c4842b31e9ee122a
sijibomi <gbemilesanmi@gmail.com>
2023-10-03 03:20:50 +0100

    chore: navigation for channels using sidebar

--- src/components/custom/navigation/NavigationSidebar.tsx
+++ src/components/custom/navigation/NavigationSidebar.tsx
@@ -5,6 +5,7 @@ import { db } from '@/lib/db';
 import { NavigationAction } from './NavigationAction';
 import { Separator } from '@/components/ui/separator';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import NavigationItem from './NavigationItem';
 
 export default async function NavigationSidebar() {
     const profile = await currentProfile();
@@ -22,14 +23,18 @@ export default async function NavigationSidebar() {
             },
         },
     });
-    
+
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-slate-800 py-3">
             <NavigationAction />
             <Separator className="h-[2px] bg-slate-800 dark:bg-slate-500 rounded-md w-12 mx-auto" />
             <ScrollArea>
                 {servers.map(server => (
-                    <div key={server.id}>{server.name}</div>
+                    <NavigationItem
+                        id={server.id}
+                        imageUrl={server.imageurl}
+                        name={server.name}
+                    />
                 ))}
             </ScrollArea>
         </div>
