981923c73d3dd6368beb0dc9a3f50c619deca239
sijibomi <gbemilesanmi@gmail.com>
2023-10-07 03:03:06 +0100

    chore; more stuff added to sidebar

--- src/components/custom/Server/ServerHeader.tsx
+++ src/components/custom/Server/ServerHeader.tsx
@@ -12,6 +12,7 @@ import {
     ChevronDown,
     PlusCircle,
     Settings,
+    Trash,
     UserPlus,
     Users,
 } from 'lucide-react';
@@ -33,7 +34,7 @@ function ServerHeader({
                 className="focus:outline-none"
                 asChild
             >
-                <button className="w-full text-sm font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-1 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+                <button className="w-full text-sm font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-1 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition ">
                     {server.name}
                     <ChevronDown className="h-5 w-5 ml-auto" />
                 </button>
@@ -64,6 +65,18 @@ function ServerHeader({
                     </DropdownMenuItem>
                 )}
                 {isModerator && <DropdownMenuSeparator />}
+                {isAdmin && (
+                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                        Delete Server
+                        <Trash className="h-4 w-4 ml-auto" />
+                    </DropdownMenuItem>
+                )}
+                {!isAdmin && (
+                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                        Leave Server
+                        <Trash className="h-4 w-4 ml-auto" />
+                    </DropdownMenuItem>
+                )}
             </DropdownMenuContent>
         </DropdownMenu>
     );
