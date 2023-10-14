d6a5c5b34fa0a2ecb630ea8834291380fe230912
sijibomi <gbemilesanmi@gmail.com>
2023-10-14 02:30:04 +0100

    chore: command key to search channels and members added

--- src/components/custom/Server/ServerSidebar.tsx
+++ src/components/custom/Server/ServerSidebar.tsx
@@ -1,14 +1,33 @@
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirectToSignIn } from '@clerk/nextjs';
-import { ChannelType } from '@prisma/client';
+import { ChannelType, MemberRole } from '@prisma/client';
 import { redirect } from 'next/navigation';
 import React from 'react';
 import ServerHeader from './ServerHeader';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import ServerSearch from './ServerSearch';
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from 'lucide-react';
 
 interface Props {
     serverId: string;
 }
+
+const iconMap = {
+    [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+    [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+    [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+    [MemberRole.GUEST]: null,
+    [MemberRole.MODERATOR]: (
+        <ShieldCheck className="h-4 w-4 ml-2 text-indigo-800" />
+    ),
+    [MemberRole.ADMIN]: (
+        <ShieldAlert className="h-4 w-4 ml-2 text-indigo-800" />
+    ),
+};
 
 async function ServerSidebar({ serverId }: Props) {
     const profile = await currentProfile();
@@ -36,7 +55,6 @@ async function ServerSidebar({ serverId }: Props) {
         },
     });
 
-
     if (!server) {
         return redirect('/');
     }
@@ -53,13 +71,56 @@ async function ServerSidebar({ serverId }: Props) {
     const members = server.members.filter(
         member => member.profileId !== profile.id
     );
-    const role = server.members.find(
-        member => member.profileId === profile.id
-    )?.role;
+    const role = server.members.find(member => member.profileId === profile.id)
+        ?.role;
 
     return (
         <nav className="flex flex-col w-full h-full text-primary dark:bg-slate-900 bg-slate-400">
             <ServerHeader server={server} role={role} />
+            <ScrollArea className="flex-1 px-3">
+                <div className="mt-2">
+                    <ServerSearch
+                        data={[
+                            {
+                                label: 'Text Channels',
+                                type: 'channel',
+                                data: textChannels.map(channel => ({
+                                    id: channel.id,
+                                    name: channel.name,
+                                    icon: iconMap[channel.type],
+                                })),
+                            },
+                            {
+                                label: 'Voice Channels',
+                                type: 'channel',
+                                data: audioChannels.map(channel => ({
+                                    id: channel.id,
+                                    name: channel.name,
+                                    icon: iconMap[channel.type],
+                                })),
+                            },
+                            {
+                                label: 'Video Channels',
+                                type: 'channel',
+                                data: videoChannels.map(channel => ({
+                                    id: channel.id,
+                                    name: channel.name,
+                                    icon: iconMap[channel.type],
+                                })),
+                            },
+                            {
+                                label: 'Members',
+                                type: 'mmeber',
+                                data: members.map(member => ({
+                                    id: member.id,
+                                    name: member.profile.name,
+                                    icon: roleIconMap[member.role],
+                                })),
+                            },
+                        ]}
+                    />
+                </div>
+            </ScrollArea>
         </nav>
     );
 }
