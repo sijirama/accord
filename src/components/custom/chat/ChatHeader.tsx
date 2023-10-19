ae3372b2f601ed9f1a56617da6a8589c17878bbf
sijibomi <gbemilesanmi@gmail.com>
2023-10-20 00:36:12 +0100

    feature:ChatPageHeader for channel done

--- src/components/custom/chat/ChatHeader.tsx
+++ src/components/custom/chat/ChatHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Hash, Menu } from 'lucide-react';
+import { Hash } from 'lucide-react';
+import MobileToggle from '../mobile-toggle';
 
 interface ChatHeaderProps {
     serverId: string;
@@ -11,9 +12,9 @@ interface ChatHeaderProps {
 function ChatHeader({ serverId, name, type, image }: ChatHeaderProps) {
     return (
         <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-            <Menu />
+            <MobileToggle serverId={serverId} />
             {type == 'channel' && (
-                <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2 " />
+                <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-1 " />
             )}
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
