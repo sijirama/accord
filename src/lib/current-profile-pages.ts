ea93e07673d99cc414fcb1f69f43643c1bfd5b0f
sijibomi <gbemilesanmi@gmail.com>
2023-10-20 23:28:17 +0100

    feat:chat input for conversations

---
+++ src/lib/current-profile-pages.ts
@@ -0,0 +1,30 @@
+import { getAuth } from '@clerk/nextjs/server';
+
+import { db } from './db';
+import { toast } from '@/components/ui/use-toast';
+import { NextApiRequest } from 'next';
+
+export const currentProfile = async (req: NextApiRequest) => {
+    const { userId } = getAuth(req);
+    if (!userId) {
+        return null;
+    }
+    let profile;
+    try {
+        profile = await db.profile.findUnique({
+            where: {
+                userId,
+            },
+        });
+    } catch (error) {
+        console.error(error, ': Error in Current Profile Function!');
+    }
+
+    if (!profile) {
+        toast({
+            description: 'No current profile found',
+        });
+    }
+
+    return profile;
+};
---
+++ src/pages/api/socket/messages.ts
@@ -0,0 +1,16 @@
+import { NextApiResponseServerIo } from '@/types';
+import { NextApiRequest } from 'next';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponseServerIo
+) {
+    if (req.method !== 'POST') {
+        return res
+            .status(405)
+            .json({
+                error: 'Method not allowed, this route only accepts POST requests',
+            });
+    }
+    
+}
