3b1026fb3a4f0392eb92c183f19b3140c6797264
sijibomi <gbemilesanmi@gmail.com>
2023-10-20 15:47:33 +0100

    feature:socket indicator

--- src/components/custom/chat/ChatHeader.tsx
+++ src/components/custom/chat/ChatHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Hash } from 'lucide-react';
 import MobileToggle from '../mobile-toggle';
 import UserAvatar from '../UserAvatar';
+import SocketIndicator from '../socket-indicator';
 
 interface ChatHeaderProps {
     serverId: string;
@@ -26,6 +27,9 @@ function ChatHeader({ serverId, name, type, image }: ChatHeaderProps) {
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
             </p>
+            <div className="ml-auto flex items-center ">
+                <SocketIndicator />
+            </div>
         </div>
     );
 }
