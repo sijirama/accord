a3b716c512f5b9998ac5adc05ae11126184e16b2
sijibomi <gbemilesanmi@gmail.com>
2023-10-22 02:27:15 +0100

    chore: query client and hook for chat messages queries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider';
 import { SocketProvider } from '@/components/providers/socket-provider';
 
 import { Toaster } from '@/components/ui/toaster';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 const font = Inter({ subsets: ['latin'] });
 
@@ -40,7 +41,7 @@ export default function RootLayout({
                     >
                         <SocketProvider>
                             <ModalProvider />
-                            {children}
+                            <QueryProvider>{children}</QueryProvider>
                         </SocketProvider>
                     </ThemeProvider>
                     <Toaster />
