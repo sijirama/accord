4d9295b5cf22cc1e41a35cfc0265fa57dc6a2ec3
sijibomi <gbemilesanmi@gmail.com>
2023-10-23 22:21:18 +0100

    chore: chat done

--- src/components/custom/chat/ChatWelcome.tsx
+++ src/components/custom/chat/ChatWelcome.tsx
@@ -22,7 +22,7 @@ function ChatWelcome({ name, type }: ChatWelcomeProps) {
 
             <p className="text-zinc-600 dark:text-zinc-400 text-sm ">
                 {type === 'channel'
-                    ? `This is the of the ${name} chat.`
+                    ? `This is the start of the ${name} chat.`
                     : `This is the start of your conversation with ${name}.`}
             </p>
         </div>
