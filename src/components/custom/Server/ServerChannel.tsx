c7207e3569bcb72b24e7a55ca2396dd10bb2cdd5
sijibomi <gbemilesanmi@gmail.com>
2023-10-26 02:01:04 +0100

    build erros

--- src/components/custom/Server/ServerChannel.tsx
+++ src/components/custom/Server/ServerChannel.tsx
@@ -26,7 +26,7 @@ function ServerChannel({ channel, server, role }: ServerChannelProps) {
     const Icon = iconMap[channel.type];
 
     const onClick = () => {
-        router.push(`/server/${params.serverId}/channels/${channel.id}`);
+        router.push(`/server/${params?.serverId}/channels/${channel.id}`);
     };
 
     const onAction = (e: React.MouseEvent, action: ModalType) => {
