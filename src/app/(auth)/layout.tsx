fdf8fd7c6432334f52bfe27cb197410366e9501c
sijibomi <gbemilesanmi@gmail.com>
2023-11-22 12:36:04 +0100

    fix: bg color in auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,15 @@
-import '@/app/globals.css'
-import type { Metadata } from 'next'
+import '@/app/globals.css';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: "Authentication for Accord",
+    title: 'Authentication for Accord',
     description: 'Identify yourself boy!',
-}
+};
 
 export default function RootLayout({
     children,
 }: {
-    children: React.ReactNode
+    children: React.ReactNode;
 }) {
-    return (
-        <div className="bg-slate-400 h-full">{children}</div>
-    )
+    return <div className="bg-slate-900 h-full">{children}</div>;
 }
