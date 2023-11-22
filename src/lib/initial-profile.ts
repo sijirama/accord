d513398c91688e02c2572bfec751546b02f160df
sijibomi <gbemilesanmi@gmail.com>
2023-11-22 12:33:35 +0100

    change: re-route to sign up.

--- src/lib/initial-profile.ts
+++ src/lib/initial-profile.ts
@@ -1,29 +1,33 @@
-import { currentUser, redirectToSignIn } from "@clerk/nextjs"
-import { type Profile } from "@prisma/client"
-import { db } from "./db"
+import { currentUser, redirectToSignIn } from '@clerk/nextjs';
+import { type Profile } from '@prisma/client';
+import { db } from './db';
+import { redirectToSignUp } from '@clerk/nextjs';
 
 export async function initialProfile(): Promise<Profile> {
-    const user = await currentUser() // get the current user
-    if (!user) { // then redirect the user to signin
-        return redirectToSignIn()
+    const user = await currentUser(); // get the current user
+    if (!user) {
+        //INFO: then redirect the user to signin
+        return redirectToSignUp();
     }
     const profile = await db.profile.findUnique({
         where: {
-            userId: user.id
-        }
-    })
-    if (profile) { // then our user has no profile, so create one
-        return profile
+            userId: user.id,
+        },
+    });
+    if (profile) {
+        //INFO: if the user has a profile
+        return profile;
     }
+    //INFO: else create a new profile
     const newProfile = await db.profile.create({
         data: {
             userId: user.id,
             name: `${user.firstName} ${user.lastName}`,
             imageUrl: user.imageUrl,
             email: user.emailAddresses[0].emailAddress,
-        }
-    })
+        },
+    });
     return newProfile; // done
 }
 
-export type ProfileType = Awaited<ReturnType<typeof initialProfile>>
+export type ProfileType = Awaited<ReturnType<typeof initialProfile>>;
